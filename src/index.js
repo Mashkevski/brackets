b737cdc07184931f9b4f791f6c1ded945d082d83
Siarhei Mashkevski <cybera@list.ru>
2019-03-15 22:22:39 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let stack = [];
+  let balanced = true;
+
+  for (let char of str) {
+    let isLeft = false;
+    for (let i = 0; i < bracketsConfig.length; i++) {
+      let isEqual = false;
+      let leftBracket = bracketsConfig[i][0];
+      let rightBracket = bracketsConfig[i][1];
+      isEqual = leftBracket === rightBracket;
+      isLeft = false;
+
+      if (stack.length === 0 && !isEqual && char === rightBracket) {
+        return false;
+      }
+
+      if (char === leftBracket) {
+        if (isEqual && stack.includes(leftBracket)) {
+          break;
+        }
+        if (isEqual) {
+          isLeft = true;
+        }
+        stack.push(rightBracket);
+        balanced = false;
+        break;
+      }
+    }
+    if (char === stack[stack.length - 1] && !isLeft) {
+      stack.pop();
+    }
+    if (stack.length === 0) {
+      balanced = true;
+    }
+  }
+  return balanced;
+};
+
